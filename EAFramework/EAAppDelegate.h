368eb30d552753937d2ac6b4530f718127f61d14
zerxon <297773034@qq.com>
2015-01-18 11:05:31 +0800

    Initial Commit

---
+++ EAFramework/EAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EAAppDelegate.h
+//  EAFramework
+//
+//  Created by developer on 15-1-18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EAFramework/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EAFramework
+//
+//  Created by developer on 15-1-18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EAAppDelegate class]));
+    }
+}
